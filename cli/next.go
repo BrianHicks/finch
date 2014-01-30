f978eca47c3b816b86d76140d07f949d43f2c41a
Brian Hicks <brian.hicks@rockfishinteractive.com>
2014-01-30 07:09:07 -0600

    fill out Next

--- cli/next.go
+++ cli/next.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	commander "code.google.com/p/go-commander"
+	"fmt"
+	"github.com/BrianHicks/finch"
 
 	"log"
 )
+
+func GetNext(tdb *finch.TaskDB, args []string) (*finch.Task, error) {
+	return tdb.GetNextSelected()
+}
 
 var Next *commander.Command = &commander.Command{
 	UsageLine: "next",
 	Short:     "display the current task",
 	Long:      `show the currently active task. This will be the most recently added selected task. To select tasks, run "select".`,
 	Run: func(cmd *commander.Command, args []string) {
-		log.Printf("next: %+v\n", args)
+		tdb, err := getTaskDB()
+		defer tdb.Close()
+		if err != nil {
+			log.Fatalf("Error opening Task database: %s\n", err)
+		}
+
+		task, err := GetNext(tdb, args)
+		if err != nil {
+			log.Fatalf("Error getting task: %s\n", err)
+		}
+
+		fmt.Printf("%s\n", task.Description)
 	},
 }
