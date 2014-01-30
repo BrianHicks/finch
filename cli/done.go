0931a457eb1a30bf06d47d88768ea1def69f4e45
Brian Hicks <brian.hicks@rockfishinteractive.com>
2014-01-30 08:33:38 -0600

    finish done

--- cli/done.go
+++ cli/done.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	commander "code.google.com/p/go-commander"
+	"fmt"
+	"github.com/BrianHicks/finch"
 
 	"log"
 )
+
+func MarkDone(tdb *finch.TaskDB, args []string) (*finch.Task, error) {
+	task, err := tdb.GetNextSelected()
+	if err != nil {
+		return task, err
+	}
+
+	task.Attrs[finch.TagSelected] = false
+	task.Attrs[finch.TagPending] = false
+
+	// TODO: callbacks to other programs here?
+
+	err = tdb.PutTasks(task)
+	if err != nil {
+		return task, err
+	}
+
+	return task, nil
+}
 
 var Done *commander.Command = &commander.Command{
 	UsageLine: "done",
 	Short:     "mark current task as done",
 	Long:      `mark the current task (from "next") as done. If you're not actually *done* with this task, use "delay"`,
 	Run: func(cmd *commander.Command, args []string) {
-		log.Printf("done: %+v\n", args)
+		tdb, err := getTaskDB()
+		defer tdb.Close()
+		if err != nil {
+			log.Fatalf("Error opening Task database: %s\n", err)
+		}
+
+		task, err := MarkDone(tdb, args)
+		if err != nil {
+			log.Fatalf("Error marking task done: %s\n", err)
+		}
+
+		fmt.Printf("Added \"%s\"\n", task.Description)
 	},
 }
